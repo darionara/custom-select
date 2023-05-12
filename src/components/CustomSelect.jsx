97a92b986a238ae1f6baf29d9c82d2bc867e86d5
Daria Ziobrowska <darianocon@gmail.com>
2023-05-12 13:03:43 +0200

    Add PropTypes

--- src/components/CustomSelect.jsx
+++ src/components/CustomSelect.jsx
@@ -1,20 +1,12 @@
 import { useState, useEffect, useRef } from 'react';
+import PropTypes from 'prop-types';
 import styles from './CustomSelect.module.scss';
 import ArrowDown from '../assets/expand_more.svg';
 import ArrowUp from '../assets/expand_less.svg';
 
-/* 
-I need some help with the last two requirements:
-- Should be able to navigate with keyboard (to focus select, open it and navigate list items with arrows)
-- should save chosen element in app state outside the component
-*/
-
-// 'options' is missing in props validation
-// why do I get this problem with the prop and how to fix it?
 function CustomSelect({ options }) {
   const [selected, setSelected] = useState(options[0]);
   const [isOpen, setIsOpen] = useState(false);
-  // it is my first time using this hook and I will be grateful if we can talk more about it during the meeting :)
   const selectRef = useRef(null);
 
   function selectOption(option) {
@@ -32,10 +24,10 @@ function CustomSelect({ options }) {
     return () => {
       document.removeEventListener('click', handleClickOutside);
     };
-  }, [selectRef]);
+  });
 
   return (
-    <>
+    <div>
       <span className={styles.label}>Choose an option:</span>
       <div className={styles.container} ref={selectRef}>
         <button className={styles.selectButton} onClick={() => setIsOpen(!isOpen)}>
@@ -46,9 +38,8 @@ function CustomSelect({ options }) {
           <ul className={styles.optionList}>
           {options.map(option => (
             <li 
-              key={option} 
-              // the selected class is not working - how to combine two classes names in one className?
-              className={`${styles.option} ${selected === option ? 'styles.selected' : ''}`} 
+              key={option}
+              className={`${styles.option} ${selected === option ? styles['option--selected'] : ''}`} 
               onClick={() => selectOption(option)}
             >
               {option}
@@ -57,9 +48,13 @@ function CustomSelect({ options }) {
         </ul>        
         )}
       </div>
-    </>
+    </div>
 
   )
+}
+
+CustomSelect.propTypes = {
+  options: PropTypes.array
 }
 
 export default CustomSelect;
