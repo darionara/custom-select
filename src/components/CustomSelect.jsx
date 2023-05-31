9e878aa801d3603f545e17ef7e7a19b2070fbbdb
Daria <darianocon@gmail.com>
2023-05-31 19:25:36 +0200

    Use keys to navigate through the options

--- src/components/CustomSelect.jsx
+++ src/components/CustomSelect.jsx
@@ -5,10 +5,12 @@ import arrowDown from '../assets/expand_more.svg';
 
 function CustomSelect({ options, selected, onSelect }) {
   const [isOpen, setIsOpen] = useState(false);
+  const [focusedIndex, setFocusedIndex] = useState(-1);
   const selectRef = useRef(null);
 
   function selectOption(option) {
     onSelect(option);
+    // after choosing the option the list is closing
     setIsOpen(false);
   }
 
@@ -24,6 +26,48 @@ function CustomSelect({ options, selected, onSelect }) {
     };
   });
 
+  function handleKeyDown(event) {
+    if (!isOpen) return;
+    switch (event.key) {
+      case 'ArrowUp': 
+        event.preventDefault();
+        setFocusedIndex((prevIndex) => prevIndex <= 0 ? options.length - 1 : prevIndex - 1);
+        break;
+      case 'ArrowDown': 
+        event.preventDefault();
+        setFocusedIndex((prevIndex) => prevIndex >= options.length - 1 ? 0 : prevIndex + 1);
+        break;
+      case 'Enter':
+        // Idk what is wrong here but to choose an option I need to press enter twice.
+        // + after choosing the option the list is not closing itself...
+        //event.preventDefault();
+        if (focusedIndex >= 0 && focusedIndex < options.length) {
+          selectOption(options[focusedIndex]);
+        }
+        break;
+      default:
+        break;
+    }
+  }
+
+  useEffect(() => {
+    if (isOpen) {
+      document.addEventListener('keydown', handleKeyDown)
+    } else {
+      document.removeEventListener('keydown', handleKeyDown)
+    }
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown)
+    }
+/*  I got a message from eslint:
+    React Hook useEffect has a missing dependency: 'handleKeyDown'. 
+    Either include it or remove the dependency array. eslintreact-hooks/exhaustive-deps */
+  }, [isOpen])
+
+  console.log(selected)
+  console.log(focusedIndex)
+
   return (
     <div>
       <span className={styles.label}>Choose an option:</span>
@@ -33,13 +77,15 @@ function CustomSelect({ options, selected, onSelect }) {
           <img src={arrowDown} className={isOpen && styles.arrowUp}></img>
         </button>
         {isOpen && (
-          <ul className={styles.optionList}>
+          <ul className={styles.optionList} onKeyDown={handleKeyDown}>
           {options.map(option => (
             <li 
               key={option}
-              className={`${styles.option} ${selected === option ? styles['option--selected'] : ''}`} 
+              className={`${styles.option} 
+                          ${selected === option ? styles['option--selected'] : ''} 
+                          ${focusedIndex === options.indexOf(option) ? styles['option--focused'] : ''}`} 
               onClick={() => selectOption(option)}
-            >
+            > 
               {option}
             </li>
           ))}
@@ -47,7 +93,6 @@ function CustomSelect({ options, selected, onSelect }) {
         )}
       </div>
     </div>
-
   )
 }
 
