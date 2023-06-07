a467b48de054288c83a40a07039a466a9beb8b66
Daria <darianocon@gmail.com>
2023-06-07 23:27:55 +0200

    Make the scroll move with arrow keys

--- src/components/CustomSelect.jsx
+++ src/components/CustomSelect.jsx
@@ -5,12 +5,13 @@ import arrowDown from '../assets/expand_more.svg';
 
 function CustomSelect({ options, selected, onSelect }) {
   const [isOpen, setIsOpen] = useState(false);
-  const [focusedIndex, setFocusedIndex] = useState(-1);
+  const [focusedIndex, setFocusedIndex] = useState(0);
   const selectRef = useRef(null);
+  const ulRef = useRef(null);
+  const liRefs = useRef([]);
 
   function selectOption(option) {
     onSelect(option);
-    // after choosing the option the list is closing
     setIsOpen(false);
   }
 
@@ -26,21 +27,30 @@ function CustomSelect({ options, selected, onSelect }) {
     };
   });
 
+  useEffect(() => {
+    const liHeight = liRefs.current[focusedIndex]?.offsetHeight;  //total height of li element
+    const scrollTop = ulRef.current?.scrollTop;                   //shows the scroll position of ul element
+    const viewport = scrollTop + ulRef.current?.offsetHeight;     //sum of scroll position of ul and it's total height
+    const liOffset = liHeight * focusedIndex;                     //total height of li element depending on the index
+    
+    console.log('focusedIndex', focusedIndex, ' viewport', viewport, ' liOffset', liOffset);
+
+    if (ulRef.current && (liOffset < scrollTop || liOffset + liHeight > viewport)) {
+      ulRef.current.scrollTop = liOffset;
+    }
+  }, [focusedIndex]);
+
   function handleKeyDown(event) {
     if (!isOpen) return;
+    event.preventDefault();
     switch (event.key) {
-      case 'ArrowUp': 
-        event.preventDefault();
-        setFocusedIndex((prevIndex) => prevIndex <= 0 ? options.length - 1 : prevIndex - 1);
+      case 'ArrowUp':
+        setFocusedIndex((prevIndex) => (prevIndex <= 0 ? options.length - 1 : prevIndex - 1));
         break;
-      case 'ArrowDown': 
-        event.preventDefault();
-        setFocusedIndex((prevIndex) => prevIndex >= options.length - 1 ? 0 : prevIndex + 1);
+      case 'ArrowDown':
+        setFocusedIndex((prevIndex) => (prevIndex >= options.length - 1 ? 0 : prevIndex + 1));
         break;
       case 'Enter':
-        // Idk what is wrong here but to choose an option I need to press enter twice.
-        // + after choosing the option the list is not closing itself...
-        //event.preventDefault();
         if (focusedIndex >= 0 && focusedIndex < options.length) {
           selectOption(options[focusedIndex]);
         }
@@ -50,56 +60,39 @@ function CustomSelect({ options, selected, onSelect }) {
     }
   }
 
-  useEffect(() => {
-    if (isOpen) {
-      document.addEventListener('keydown', handleKeyDown)
-    } else {
-      document.removeEventListener('keydown', handleKeyDown)
-    }
-
-    return () => {
-      document.removeEventListener('keydown', handleKeyDown)
-    }
-/*  I got a message from eslint:
-    React Hook useEffect has a missing dependency: 'handleKeyDown'. 
-    Either include it or remove the dependency array. eslintreact-hooks/exhaustive-deps */
-  }, [isOpen])
-
-  console.log(selected)
-  console.log(focusedIndex)
-
   return (
     <div>
       <span className={styles.label}>Choose an option:</span>
       <div className={styles.container} ref={selectRef}>
-        <button className={styles.selectButton} onClick={() => setIsOpen(!isOpen)}>
+        <button className={styles.selectButton} onClick={() => setIsOpen(!isOpen)} onKeyDown={handleKeyDown}>
           <span>{selected}</span>
-          <img src={arrowDown} className={isOpen && styles.arrowUp}></img>
+          <img src={arrowDown} className={isOpen ? styles.arrowUp : ''} alt="Arrow" />
         </button>
         {isOpen && (
-          <ul className={styles.optionList} onKeyDown={handleKeyDown}>
-          {options.map(option => (
-            <li 
-              key={option}
-              className={`${styles.option} 
-                          ${selected === option ? styles['option--selected'] : ''} 
-                          ${focusedIndex === options.indexOf(option) ? styles['option--focused'] : ''}`} 
-              onClick={() => selectOption(option)}
-            > 
-              {option}
-            </li>
-          ))}
-        </ul>        
+          <ul className={styles.optionList} ref={ulRef}>
+            {options.map((option, index) => (
+              <li
+                key={index}
+                className={`${styles.option} 
+                            ${selected === option ? styles['option--selected'] : ''}
+                            ${focusedIndex === index ? styles['option--focused'] : ''}`}
+                onClick={() => selectOption(option)}
+                ref={(li) => (liRefs.current[index] = li)}
+              >
+                {option}
+              </li>
+            ))}
+          </ul>
         )}
       </div>
     </div>
-  )
+  );
 }
 
 CustomSelect.propTypes = {
   options: PropTypes.array,
   selected: PropTypes.string,
   onSelect: PropTypes.func
-}
+};
 
 export default CustomSelect;
